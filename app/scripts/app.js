7f707c7f2b29098ef9bf4c18ed802f9b1e4bdf23
George Woodard <gwoodard@bulldogs.aamu.edu>
2015-11-18 14:59:26 -0600

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
 (function () {
     document.addEventListener("deviceready", function () {
+        //Adds the image 08.jpg to the list view when the Add button is clicked.
         window.listview = kendo.observable({
             addImage: function () {
                 $("#images")
@@ -10,6 +11,8 @@
         var app = new kendo.mobile.Application(document.body, {
             skin: "nova"
         });
+        //Each image that is stored in the images folder, are called in the dataSource array.
+        //Then the images are called and displayed on the ListView of the applicaiton.
         $("#images").kendoMobileListView({
             dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
             template: "<img src = '#: data #'>"
